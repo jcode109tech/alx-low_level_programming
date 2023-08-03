eabf96361a9b17a9e8cd8614961a89c96669733a
jcode109tech <jedydiahr9@gmail.com>
2023-08-03 13:29:31 -0700

    Advance

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -11,7 +11,7 @@ int _check_pal(char *s, int i, int len);
 
 int is_palindrome(char *s)
 {
-	if(*s == 0)
+	if (*s == 0)
 		return (0);
 
 	return (_check_pal(s, 0,  _strlen_pal(s)));
@@ -26,7 +26,7 @@ int is_palindrome(char *s)
 
 int _strlen_pal(char *s)
 {
-	if(*s == '\0')
+	if (*s == '\0')
 		return (0);
 
 	return (1 + _strlen_pal(s + 1));
@@ -44,8 +44,8 @@ int _check_pal(char *s, int i, int len)
 	if (s[i] != s[len - 1])
 		return (0);
 	else if (i > len)
-		return(1);
+		return (1);
 
-	return(_check_pal(s, i + 1, len - 1));
+	return (_check_pal(s, i + 1, len - 1));
 
 }
