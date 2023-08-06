96555569f77f8a6606ea7d305d33fe6eeb849081
jcode109tech <jedydiahr9@gmail.com>
2023-08-06 21:17:49 +0300

    Libraries

---
+++ 0x09-static_libraries/2-strncpy.c
@@ -0,0 +1,31 @@
+#include"main.h"
+
+/**
+ * *_strncpy -  a function that copies a string.
+ *
+ * @dest: - string pointer.
+ * @src: - string pointer.
+ *
+ * Return: copy of string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i, j;
+
+	i = j = 0;
+
+	while (j < n && src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	while (j < n)
+	{
+		dest[j] = '\0';
+		j++;
+	}
+
+	return (dest);
+}
