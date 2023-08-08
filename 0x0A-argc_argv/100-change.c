ddbcb262fa152086718c8b0ec3dba91f8b0fc5fd
jcode109tech <jedydiahr9@gmail.com>
2023-08-08 20:59:47 +0300

    Args_Arvs[]

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,22 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
 /**
- * Write a program that prints the minimum number of coins to make change for an amount of money.
+ * main - prints the minimum number of coins to make change for an amount.
+ * of money.
+ * @argc: no: command line arguments.
+ * @argv: array
+ * Return: 0 Always
+ */
+int main(int argc, char *argv[])
+{
+int cents, ncoins = 0;
 
-Usage: ./change cents
-where cents is the amount of cents you need to give back
-if the number of arguments passed to your program is not exactly 1, print Error, followed by a new line, and return 1
-you should use atoi to parse the parameter passed to your program
-If the number passed as the argument is negative, print 0, followed by a new line
-You can use an unlimited number of coins of values 25, 10, 5, 2, and 1 cent
-julien@ubuntu:~/0x0A. argc, argv$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 100-change.c -o change
-julien@ubuntu:~/0x0A. argc, argv$ ./change 
-Error
-julien@ubuntu:~/0x0A. argc, argv$ ./change 10
-1
-julien@ubuntu:~/0x0A. argc, argv$ ./change 100
-4
-julien@ubuntu:~/0x0A. argc, argv$ ./change 101
-5
-julien@ubuntu:~/0x0A. argc, argv$ ./change 13
-3
-julien@ubuntu:~/0x0A. argc, argv
-*/
+if (argc == 1 || argc > 2)
+{
+printf("Error\n");
+return (1);
+}
+
+cents = atoi(argv[1]);
+
+while (cents > 0)
+{
+if (cents >= 25)
+cents -= 25;
+else if (cents >= 10)
+cents -= 10;
+else if (cents >= 5)
+cents -= 5;
+else if (cents >= 2)
+cents -= 2;
+else if (cents >= 1)
+cents -= 1;
+ncoins += 1;
+}
+printf("%d\n", ncoins);
+return (0);
+}
