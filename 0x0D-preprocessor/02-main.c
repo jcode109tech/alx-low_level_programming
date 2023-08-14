ac39e04276c569623ec5cc0dc09e6b99f8e2ec10
jcode109tech <jedydiahr9@gmail.com>
2023-08-14 17:23:24 +0300

    0x0D-preprocessor

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - e a program that prints the name of the file it was compiled
+*
+* Return: 0 Success
+*/
+
+int main(void)
+{
+printf("File Name : %s\n", __FILE__);
+
+return (0);
+}
