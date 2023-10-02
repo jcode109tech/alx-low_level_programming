3d80c66066b5a466b81fac11422b58a765470e88
jcode109tech <jedydiahr9@gmail.com>
2023-10-02 21:35:14 +0300

    3-cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,51 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <errno.h>
 
-#define BUFFER_SIZE 1024
+char *create_buffer(char *file);
+void close_file(int fd);
 
 /**
- * exitWithError - function for error
- * @error_message: error string message
- * @exit_code: exits program
+ * create_buffer - Allocates 1024 bytes for a buffer.
+ * @file: The name of the file buffer is storing chars for.
+ *
+ * Return: A pointer to the newly-allocated buffer.
  */
-
-void exitWithError(const char *error_message, int exit_code)
+char *create_buffer(char *file)
 {
-	dprintf(STDERR_FILENO, "%s\n", error_message);
-	exit(exit_code);
-}
-/**
- * openFile - A function that open file
- * @file_path: path to the file we want ot read from or write into it
- * @flags: flags
- * @mode: file permisson
- **/
+	char *buffer;
 
-int openFile(const char *file_path, int flags, int mode)
-{
-	int fd = open(file_path, flags, mode);
+	buffer = malloc(sizeof(char) * 1024);
 
-	if (fd == -1)
+	if (buffer == NULL)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't open file %s\n", file_path);
-		exit(98);
+		dprintf(STDERR_FILENO,
+			"Error: Can't write to %s\n", file);
+		exit(99);
 	}
-	return (fd);
+
+	return (buffer);
 }
 
 /**
- * closeFile - close file
- * @fd: fd no:
+ * close_file - Closes file descriptors.
+ * @fd: The file descriptor to be closed.
  */
-
-void closeFile(int fd)
+void close_file(int fd)
 {
-	if (close(fd) == -1)
+	int c;
+
+	c = close(fd);
+
+	if (c == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
@@ -53,55 +44,71 @@ void closeFile(int fd)
 }
 
 /**
- * main - main program
- * @argc: no: of argument
- * @argv: array
- * Return: 0 Success
- **/
-
+ * main - Copies the contents of a file to another file.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: 0 on success.
+ *
+ * Description: If the argument count is incorrect - exit code 97.
+ * If file_from does not exist or cannot be read - exit code 98.
+ * If file_to cannot be created or written to - exit code 99.
+ * If file_to or file_from cannot be closed - exit code 100.
+ */
 int main(int argc, char *argv[])
 {
-	const char *file_from = argv[1];
-	const char *file_to = argv[2];
-
-	int fd_from = openFile(file_from, O_RDONLY, 0600);
-	int fd_to = openFile(file_to,
-	            O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP);
-
-	char buffer[BUFFER_SIZE];
-	ssize_t bytes_read, bytes_written;
+	int from, to, r, w;
+	char *buffer;
 
 	if (argc != 3)
-		exitWithError("Usage: cp file_from file_to", 97);
-
-	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 	{
-		if (fd_from == -1 || bytes_read == -1)
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	buffer = create_buffer(argv[2]);
+	from = open(argv[1], O_RDONLY);
+	r = read(from, buffer, 1024);
+	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+	do {
+		if (from == -1 || r == -1)
 		{
 			dprintf(STDERR_FILENO,
-					"Error: Can't read from file %s\n", argv[1]);
+				"Error: Can't read from file %s\n", argv[1]);
+			free(buffer);
 			exit(98);
 		}
-		bytes_written = write(fd_to, buffer, bytes_read);
 
-		if (bytes_written == -1)
+		w = write(to, buffer, r);
+		if (to == -1 || w == -1)
 		{
 			dprintf(STDERR_FILENO,
-			        "Error: Can't write to destination file %s\n", file_to);
+				"Error: Can't write to %s\n", argv[2]);
+			free(buffer);
 			exit(99);
 		}
-	}
 
-	if (bytes_read == -1)
-	{
-		dprintf(STDERR_FILENO,
-		                "Error: Can't read from source file %s\n", file_from);
-		exit(98);
-	}
+		r = read(from, buffer, 1024);
+		to = open(argv[2], O_WRONLY | O_APPEND);
 
-	closeFile(fd_from);
-	closeFile(fd_to);
+	} while (r > 0);
+
+	free(buffer);
+	close_file(from);
+	close_file(to);
 
 	return (0);
 }
+
+
+
+
+
+
+
+
+
+
+
 
